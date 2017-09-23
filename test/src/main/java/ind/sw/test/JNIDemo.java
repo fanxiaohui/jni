9b9108164359f2d6e81f1c3be5183cc7776f40c0
asteria <souled3000@163.com>
2017-09-23 22:26:12 +0800

    2

--- test/src/main/java/ind/sw/test/JNIDemo.java
+++ test/src/main/java/ind/sw/test/JNIDemo.java
@@ -1,10 +1,13 @@
 package ind.sw.test;
 
 public class JNIDemo {
+	static{
+		System.out.println(System.getProperty("java.library.path"));
+		System.loadLibrary("JNIDemo");
+	}
 	public native void sayHello();  
     public static void main(String[] args){  
         //调用动态链接库  
-        System.loadLibrary("JNIDemo");  
         JNIDemo jniDemo = new JNIDemo();  
         jniDemo.sayHello();  
     }  
